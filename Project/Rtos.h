255463f4da2d72e2ed22c6f6d56b433ff9bfc9ca
sylsit <sitkowskis@interia.pl>
2021-01-17 21:19:02 +0100

    GroupEvent added. One Touch Semaphore removed.

--- Project/Rtos.h
+++ Project/Rtos.h
@@ -5,6 +5,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include <string>
+#include "Utils.h"
 #include <stdint.h>
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -23,25 +24,38 @@ class Rtos
         static inline Rtos * instance = nullptr;
 
     public:
+        enum class EEventGroup : uint32_t
+        {
+            eFirst  = ONE,
+            eSecond = TWO,
+        };
+
         typedef void * TaskHandle;
         typedef void (* TaskFunctionType)(void *);
-
-        static void      SetInstance          (Rtos * v_instance) { instance = v_instance; }
-        static Rtos *    GetInstance          (void) { return instance; }
 
         Rtos () = default;
         virtual ~Rtos () = default;
 
-        virtual void     DelayInMs            (const uint32_t v_ms)                  = 0;
-        virtual bool     GiveSemaphoreFromISR (const std::string_view v_name)        = 0;
-        virtual bool     TakeSemaphore        (const std::string_view v_name)        = 0;
-        virtual uint32_t GetCurrentHeapSize   (void)                                 = 0;
-        virtual uint32_t GetCurrentStackSize  (const std::string_view v_name)        = 0;
-        virtual uint32_t TaskCreate           (TaskFunctionType       v_taskFuncion,
-                                               const std::string_view v_taskName,
-                                               const uint32_t         v_stackDepth,
-                                               const uint32_t         v_priority,
-                                               TaskHandle             v_taskHandle)  = 0;
+        static void      SetInstance              (Rtos * v_instance            ) { instance = v_instance; }
+        static Rtos *    GetInstance              (void                         ) { return instance;       }
+
+        virtual void     DelayInMs                (const uint32_t v_ms)            = 0;
+        virtual bool     TakeSemaphore            (std::string_view v_name)        = 0;
+        virtual bool     GiveSemaphoreFromISR     (std::string_view v_name)        = 0;
+        virtual void     SetBitsEventGroup        (std::string_view v_name,
+                                                   const EEventGroup v_eBit)       = 0;
+        virtual void     SetBitsEventGroupFromISR (std::string_view v_name,
+                                                   const EEventGroup v_eBit)       = 0;
+        virtual void     ClearBitsEventGroup      (std::string_view v_name,
+                                                   const EEventGroup v_eBit)       = 0;
+        virtual uint32_t WaitBitsEventGroup       (std::string_view v_name)        = 0;
+        virtual uint32_t GetCurrentHeapSize       (void)                           = 0;
+        virtual uint32_t GetCurrentStackSize      (std::string_view v_name)        = 0;
+        virtual uint32_t TaskCreate               (TaskFunctionType v_taskFuncion,
+                                                   std::string_view v_taskName,
+                                                   const uint32_t   v_stackDepth,
+                                                   const uint32_t   v_priority,
+                                                   TaskHandle       v_taskHandle)  = 0;
 };
 ///////////////////////////////////////////////////////////////////////////////
 /////////////////////////////// END OF FILE ///////////////////////////////////
