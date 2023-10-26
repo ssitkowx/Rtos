c42b70a7112d442e8260c39f52c21d314711b918
ssitkowx <sitkowskis@interia.pl>
2023-10-26 20:44:31 +0200

    New function added

--- Project/Rtos.h
+++ Project/Rtos.h
@@ -27,13 +27,17 @@ class Rtos
         Rtos & operator=(DERIVED_TYPE && vRtos)      = delete;
         virtual ~Rtos   ()                           = default;
 
-        static void           SetInst (DERIVED_TYPE * const vInstance) { inst = vInstance; }
-        static DERIVED_TYPE * GetInst (void              )             { return inst;      }
+        static void           SetInst              (DERIVED_TYPE * const vInstance) { inst = vInstance; }
+        static DERIVED_TYPE * GetInst              (void                          ) { return inst;      }
 
-        void                  Init       (void)                        { derivedType.Init       ();    }
-        void                  DeInit     (void)                        { derivedType.DeInit     ();    }
-        void                  TaskDelete (void)                        { derivedType.TaskDelete ();    }
-        void                  DelayInMs  (const uint32_t vMs)          { derivedType.DelayInMs  (vMs); }
+        void                  Init                 (void)                           { derivedType.Init       ();                       }
+        void                  DeInit               (void)                           { derivedType.DeInit     ();                       }
+        void                  TaskDelete           (void)                           { derivedType.TaskDelete ();                       }
+        uint16_t              GetFreeHeapSize      (void)                           { return derivedType.GetFreeHeapSize ();           }
+        void                  DelayInMs            (const uint32_t vMs)             { derivedType.DelayInMs (vMs);                     }
+        bool                  TakeSemaphore        (std::string_view vName)         { return derivedType.TakeSemaphore (vName);        }
+        uint16_t              GetFreeStackSize     (std::string_view vTaskName)     { return derivedType.GetFreeStackSize (vTaskName); }
+        bool                  GiveSemaphoreFromISR (std::string_view vName)         { return derivedType.GiveSemaphoreFromISR (vName); }
 };
 
 ///////////////////////////////////////////////////////////////////////////////
